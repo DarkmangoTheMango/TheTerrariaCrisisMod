88bd3bf31e045db54fdbb9413ec28c39b838835d
Darkmango <DarkmangoTheMango@gmail.com>
2026-05-19 21:57:05 -0700

    Update GlobalReferences.cs

--- GlobalReferences.cs
+++ GlobalReferences.cs
@@ -1,1 +1,6 @@
+global using Terraria.ModLoader;
+global using Terraria;
+global using Microsoft.Xna.Framework;
+global using Microsoft.Xna.Framework.Graphics;
+global using System;
 global using Extras = Crisis.Core.AssetReferences.Assets.Images.Extras;
