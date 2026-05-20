faf2fbde701d9f1fbdd455e907ab5bc870ef8c42
Darkmango <DarkmangoTheMango@gmail.com>
2026-05-20 00:06:57 -0700

    feat: update global references

--- GlobalReferences.cs
+++ GlobalReferences.cs
@@ -3,4 +3,7 @@ global using Terraria;
 global using Microsoft.Xna.Framework;
 global using Microsoft.Xna.Framework.Graphics;
 global using System;
+global using Crisis.Common;
+global using Terraria.Audio;
+global using Terraria.ID;
 global using Extras = Crisis.Core.AssetReferences.Assets.Images.Extras;
